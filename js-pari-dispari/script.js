166cf704fba6e1a86e90d47bb171892f66c3d061
Damiana-Arangio <arangio.damiana@gmail.com>
2025-09-02 22:45:36 +0200

    Implementazione "js-pari-dispari" completata

--- js-pari-dispari/script.js
+++ js-pari-dispari/script.js
@@ -6,5 +6,44 @@
   Sommiamo i due numeri
   Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
   Dichiariamo chi ha vinto.
-  
+
 */
+
+// INPUT
+
+  // Scelta utente -> pari/dispari
+  let sceltaPariDispariUtente = (prompt("SCEGLI: \n Pari \n Dispari "));
+  console.log("Scelta utente: ", sceltaPariDispariUtente);
+
+  // Scelta utente -> numero
+    const numUtente = parseInt(prompt("Scegli un intero tra 1 e 5"));
+
+// Generazione numero random per computer
+const numComputer = Math.floor(Math.random() * 5) +1;
+
+// Chiamata funzione per verifica somma pari/dispari
+const sommaIsPari = sommaPariDispari(numUtente, numComputer);
+console.log("Somma is pari: ", sommaIsPari);
+
+// OUTPUT: vincitore
+if ( (sommaIsPari && sceltaPariDispariUtente === "pari") || 
+     (!sommaIsPari && sceltaPariDispariUtente === "dispari") ) {
+
+    console.log("Hai vinto!");
+  }
+else {
+  console.log("Hai perso!");
+}
+
+
+
+
+
+// FUNZIONE
+function sommaPariDispari(numeroUtente, numeroComputer) {
+  
+  const somma = numeroUtente + numeroComputer;
+  console.log("Somma = " + numeroUtente + " + " + numeroComputer + " = " + somma);
+
+  return somma % 2 === 0;
+}
