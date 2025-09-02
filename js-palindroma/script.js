b735849a2b54fbe451832e56dbefdb5d5a3e4e42
Damiana-Arangio <arangio.damiana@gmail.com>
2025-09-02 20:10:00 +0200

    Implementazione "js-palindroma" completata

--- js-palindroma/script.js
+++ js-palindroma/script.js
@@ -5,3 +5,31 @@
   Creare una funzione per capire se la parola inserita è palindroma
 
 */
+
+// INPUT: richiesta inserimento parola
+const parola = prompt("Inserisci una parola");
+
+// Chiamata funzione che controlla se parola è palindroma
+const ispalindroma = checkPalindroma(parola);
+
+// OUTPUT
+if(ispalindroma) {
+  console.log("La parola " + parola + " è palindroma");
+}
+
+else {
+  console.log("La parola " + parola + " non è palindroma");
+}
+
+
+
+// FUNZIONE 
+function checkPalindroma(stringa) {
+  stringa = stringa.toLowerCase();
+  let reverseStringa = stringa.split("").reverse().join(""); /* 
+                                                                - Trasformo stringa in array con split 
+                                                                - La inverto con reverse
+                                                                - Ricompongo la stringa con join
+                                                              */
+  return stringa === reverseStringa;
+}
